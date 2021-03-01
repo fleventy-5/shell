f3488856061668e0782e0ac9892904571e4eb37a
fleventy-5 <78148524+fleventy-5@users.noreply.github.com>
2021-03-01 19:19:51 +0530

    Add files via upload

---
+++ ksh.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <editline/readline.h>
+
+
+char **get_input(char *input){
+	char **command = malloc(8 *sizeof(char *));
+	char *seperator = " ";
+	char *parsed;
+	
+	int index = 0;
+	parsed = strtok(input, seperator);
+	while(parsed!=NULL){
+		command[index] = parsed;
+		index++;
+
+		parsed = strtok(NULL,seperator);
+	}
+
+	command[index] = NULL;
+	return command;
+}
+
+
+int main(){
+	printf("Fluffy Shell version 0.0  CTRL+ C to exit\n");
+	char **command;
+	char *input;
+	pid_t child_pid;
+	int stat_loc;
+
+	char *clr[] = {"clear",NULL};
+	child_pid = fork();
+	if(child_pid ==0){
+		execvp(clr[0],clr);
+	} else {
+		waitpid(child_pid, &stat_loc,WUNTRACED);
+	}
+
+	while(1){
+		input = readline("ksh$");
+		command = get_input(input);
+
+		child_pid  = fork();
+		if(child_pid ==0){
+			execvp(command[0],command);
+			printf("Command does not exist\n");
+		} else {
+			waitpid(child_pid, &stat_loc, WUNTRACED);
+		}
+		free(input);
+		free(command);
+	}
+	return 0;
+}
